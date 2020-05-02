b808bec3bd7fc4ffb6d8ccf9346121ca75bb80bf
Narendra Joshi <narendraj9+home@gmail.com>
2020-05-02 23:21:50 +0200

    Increase the space used for sender's name in Summary line format.

--- etc/gnus-config.el
+++ etc/gnus-config.el
@@ -80,7 +80,7 @@ buffer."
 
 ;; (setq gnus-user-date-format-alist '((t . "%Y %b %d (%H:%M)")))
 (setq gnus-summary-display-arrow t
-      gnus-summary-line-format "{%U} %2B%(%-15,15f  %) %s\n")
+      gnus-summary-line-format "{%U} %2B%(%-18,18f  %) %s\n")
 
 ;; All threads where I am being talked to directly should be in
 ;; Primary. Rest should be split.
